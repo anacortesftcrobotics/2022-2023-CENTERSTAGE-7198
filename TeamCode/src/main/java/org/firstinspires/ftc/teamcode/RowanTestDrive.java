3e59253ba7f480ba97cc25f9436832dd2f133aa5
KaiG1024 <grignkai000@stu.asd103.org>
2023-09-15 17:18:12 -0700

    Created RowanTestDrive class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RowanTestDrive.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class RowanTestDrive extends OpMode {
+
+    DcMotor FrontLeft;
+    DcMotor FrontRight;
+    DcMotor BackLeft;
+    DcMotor BackRight;
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
