a73c9811cc0855b4f14fa5e1729ae6c5cd424351
KaiG1024 <grignkai000@stu.asd103.org>
2023-09-18 17:10:45 -0700

    Added macanum X chassis function and controller input

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RowanTestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RowanTestDrive.java
@@ -1,21 +1,53 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
 
+@TeleOp
 public class RowanTestDrive extends OpMode {
 
-    DcMotor FrontLeft;
-    DcMotor FrontRight;
-    DcMotor BackLeft;
-    DcMotor BackRight;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    BNO055IMU imu;
+
     @Override
     public void init() {
-
+        frontLeft = hardwareMap.get(DcMotor.class, "leftFront");
+        frontRight = hardwareMap.get(DcMotor.class, "rightFront");
+        backLeft = hardwareMap.get(DcMotor.class, "leftBack");
+        backRight = hardwareMap.get(DcMotor.class, "rightBack");
+        //reverses direction of wheels (left)
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
+        mecanumX();
+        telemetry.update();
+    }
 
+    private void mecanumX() {
+        double forwards = -gamepad1.left_stick_y;
+        double sideways = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
+        double denominator = Math.max(Math.abs(forwards) + Math.abs(sideways) + Math.abs(rotate), 1);
+
+        //telemetry.addData("Forward", forwards);
+
+        //does math for mechanim chassis
+        frontLeft.setPower((forwards + sideways + rotate) / denominator);
+        frontRight.setPower((forwards - sideways -rotate)  / denominator);
+        backLeft.setPower((forwards - sideways + rotate)  / denominator);
+        backRight.setPower((forwards + sideways - rotate)  / denominator);
+
+        //telemetry.addData("FL", frontLeft.getPower());
     }
 }
