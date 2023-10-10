80fe5f6102b01955b6ef6259edcc7309c811d075
Blue Mink <info@anacortesrobotics.org>
2023-10-09 17:22:22 -0700

    Odometry ready for tuning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/logancode/TestOdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/logancode/TestOdo.java
@@ -21,23 +21,63 @@ public class TestOdo extends LinearOpMode {
     public void runOpMode()
     {
         mapHardware();
+        resetDriveEncoder();
 
+        kaiOdo = new Odo1(39.37,29.21,3.5,8192);
+
+        waitForStart();
+        while(opModeIsActive())
+        {
+            kaiOdo.setEncoderPos(-encoderLeft.getCurrentPosition(),
+                    encoderRight.getCurrentPosition(),
+                    encoderBack.getCurrentPosition());
+            telemetry();
+        }
     }
 
     public void mapHardware()
     {
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFront = hardwareMap.get(DcMotor.class, "frontRight");
+        leftBack = hardwareMap.get(DcMotor.class, "backLeft");
+        rightBack = hardwareMap.get(DcMotor.class, "backRight");
 
-        encoderBack = hardwareMap.get(DcMotor.class, "encoderBack");
-        encoderLeft = hardwareMap.get(DcMotor.class, "encoderLeft");
-        encoderRight = hardwareMap.get(DcMotor.class, "encoderRight");
+        encoderBack = rightFront;
+        encoderLeft = leftBack;
+        encoderRight = leftFront;
 
         //colorMan = hardwareMap.get(ColorRangeSensor.class, "colorSensor");
 
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
+
+    public void telemetry()
+    {
+        telemetry.addLine("Internal Position:");
+        telemetry.addData("(X, Y, Degrees)", Math.round(kaiOdo.getX() *10)/10d + " : " + Math.round(kaiOdo.getY() *10)/10d + " : " + Math.round(kaiOdo.getHDeg() *10)/10d);
+        telemetry.update();
+    }
+
+    public void resetDriveEncoder()
+    {
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 }
