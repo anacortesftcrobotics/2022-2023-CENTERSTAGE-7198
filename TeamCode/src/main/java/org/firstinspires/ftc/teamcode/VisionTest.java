66bd4ac56aa70d07755f688c35d8dbb136070846
Blue Mink <info@anacortesrobotics.org>
2023-10-22 13:10:33 -0700

    Team prop and Vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+public class VisionTest extends OpMode {
+
+    // Field definitions
+    VisionPortal visionPortal;
+
+    // Initialization
+    @Override
+    public void init() {
+
+    }
+
+    // Looping sequence of events
+    @Override
+    public void loop() {
+
+    }
+}
