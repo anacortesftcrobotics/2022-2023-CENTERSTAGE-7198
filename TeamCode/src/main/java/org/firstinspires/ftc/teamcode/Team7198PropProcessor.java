1010efcc6244c7137ad204b787411567d92d3065
Blue Mink <info@anacortesrobotics.org>
2023-11-05 09:29:32 -0800

    img proc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team7198PropProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team7198PropProcessor.java
@@ -4,7 +4,11 @@ import android.graphics.Canvas;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Mat;
-//import org.opencv.
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.objdetect.*;
+import org.opencv.features2d.*;
+import org.opencv.imgproc.*;
 
 public class Team7198PropProcessor implements VisionProcessor
 {
@@ -18,6 +22,15 @@ public class Team7198PropProcessor implements VisionProcessor
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
+
+        Mat gray = new Mat(frame.rows(),frame.cols(),frame.type());
+        Mat edges = new Mat(frame.rows(),frame.cols(),frame.type());
+        Mat dst = new Mat(frame.rows(),frame.cols(),frame.type(), new Scalar(0));
+        Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+
+        Imgproc.blur(gray,edges,new Size(3,3));
+        Imgproc.Canny(edges,edges,100,300);
+        frame.copyTo(dst,edges);
 
 
         return null;
